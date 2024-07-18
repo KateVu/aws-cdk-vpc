38cdd78cc4555989ef1e7a2ae84051ef312afdde
KateVu <tuquyen87@gmail.com>
2024-07-18 23:42:06 +1000

    fix get account id via pipeline

--- bin/index.ts
+++ bin/index.ts
@@ -12,9 +12,13 @@ const accountFileName = 'aws_account.yaml'
 const envName = process.env.ENVIRONMENT_NAME || 'kate'
 const accountName = process.env.ACCOUNT_NAME || 'sandpit1'
 const region = process.env.REGION || 'ap-southeast-2'
+const aws_account_id = process.env.AWS_ACCOUNT_ID || 'none'
 
 //Get aws account id
-const accountId = getAccountId(accountName, configFolder, accountFileName)
+let accountId = aws_account_id
+if (aws_account_id == 'none') {
+  accountId = getAccountId(accountName, configFolder, accountFileName)
+}
 const app = new cdk.App();
 const vpcStack = new VPCStack(app, 'VpcStack', {
   stackName: `vpc-${envName}`,
